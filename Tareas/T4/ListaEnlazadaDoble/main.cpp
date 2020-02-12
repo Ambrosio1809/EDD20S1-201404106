bb49547efa413f00cfd50e44f80ad7379d78b5a5
Ambrosio1809 <3001052740101@ingenieria.usac.edu.gt>
2020-02-11 23:34:46 -0600

    Tarea4

--- Tareas/T4/ListaEnlazadaDoble/main.cpp
+++ Tareas/T4/ListaEnlazadaDoble/main.cpp
@@ -1,37 +1,142 @@
 #include <iostream>
 
 using namespace std;
+
 class NodoLD{
     public: char dato;
     NodoLD*Siguiente;
     NodoLD*Anterior;
-};
+}*primero,*ultimo;
 
-void InsertarLD(NodoLD*,char);
-void EliminarLD(NodoLD*);
-
+void InsertarLD(char);
+void mostrar();
+void EliminarLD();
+void buscarN();
+void menu();
 
 int main()
 {
-    NodoLD *lista  = NULL;
-    cout << "Hello world!" << endl;
+    menu();
     return 0;
 }
 
-void InsertarLD(NodoLD* lista, char x){
+void InsertarLD(char x){
     NodoLD *nuevo = new NodoLD();
     nuevo->dato=x;
-    NodoLD *aux1=lista;
 
-    if(lista==aux1){
-            lista = nuevo;
-            //nuevo->Siguiente=aux1;
-            //aux1->Anterior = nuevo;
+    if(primero==NULL){
+            primero = nuevo;
+            primero->Siguiente=NULL;
+            primero->Anterior=NULL;
+            ultimo=primero;
     }else{
-        lista->Siguiente=nuevo;
-        aux1= nuevo;
-        aux1->Anterior=lista;
+        ultimo->Siguiente=nuevo;
+        nuevo->Siguiente=NULL;
+        nuevo->Anterior=ultimo;
+        ultimo = nuevo;
     }
-    cout<<"Nodo Ingresado"
+    cout<<"Nodo Ingresado";
+}
+void mostrar(){
+    NodoLD* actual = new NodoLD();
+    actual = primero;
+
+    if(primero!=NULL){
+            while(actual!=NULL){
+                    cout<<"\n"<<actual->dato;
+                    actual=actual->Siguiente;
+            }
+
+    }else{
+        cout<<"\n lista vacia";
+    }
+
 }
 
+void EliminarLD(){
+    NodoLD*actual = new NodoLD();
+    actual = primero;
+    NodoLD*anterior = new NodoLD();
+    anterior = NULL;
+
+    if(primero!=NULL){
+
+            while(actual!=NULL){
+
+                    if(actual==ultimo){
+                            cout<<"nodo ultimo localizado \n";
+                            anterior->Siguiente=NULL;
+                            ultimo = anterior;
+                            cout<<"Nodo eliminado \n";
+                    }
+            anterior=actual;
+            actual=actual->Siguiente;
+            }
+
+
+    }else{
+        cout<<"lista vacia \n";
+    }
+}
+void buscarN(char NBuscado){
+    NodoLD*actual = new NodoLD();
+    actual = primero;
+    bool encontrado = false;
+    if(primero!=NULL){
+            while(actual!=NULL && encontrado !=true){
+                    if(actual->dato==NBuscado){
+                        cout<<"Nodo encontrado en la lista;";
+                        encontrado = true;
+                    }
+                    actual=actual->Siguiente;
+
+            }
+            if(!encontrado){
+                cout<<"Nodo no encontrado\n";
+            }
+
+    }else{
+        cout<<"\n la lista se encuentra vacia";
+    }
+}
+
+void menu(){
+    int seleccion;
+    char dato;
+    string cadena;
+    do{
+            cout<<"Bienvenido, Seleccione la opción que desee:\n";
+            cout<<"1.Insertar:\n";
+            cout<<"2.Eliminar\n";
+            cout<<"3.Buscar\n";
+            cout<<"4.Mostrar\n";
+            cout<<"5.Salir\n";
+            cin>>seleccion;
+            switch(seleccion){
+                case 1:
+                    cout<<"por favor ingrese los valores a insertar en la lista:\n";
+                    cout<<"caracter:\n";
+                    cin>>dato;
+                    InsertarLD(dato);
+                break;
+                case 2:
+                cout<<"eliminando nodo ultimo\n";
+                EliminarLD();
+                break;
+                case 3:
+                    cout<<"favor ingresar el dato a buscar:\n";
+                    cin>>cadena;
+                    buscarN(dato);
+                break;
+                case 4:
+                    cout<<"El resultado de la lista es:\n";
+                    mostrar();
+                break;
+                case 5:
+                    cout<<"hasta luego";
+                break;
+            }
+
+    }while(seleccion!=5);
+    system("pause");
+}
